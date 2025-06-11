04a85b0f584234333134088927e12ba07431ed0f
Corbin Wolf <corbinwolf300@gmail.com>
2025-06-11 15:28:51 -0700

    feat(Footer.jsx): add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__developer">Developed by Corbin Wolf</p>
+      <p className="footer__year">2022</p>
+    </footer>
+  );
+}
+
+export default Footer;
